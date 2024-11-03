aff1ddd762f6868f134f3e4b02d6c8a1ea7e168d
Neel Patel <102969955+neelthepatel8@users.noreply.github.com>
2024-11-02 22:37:09 -0400

    Neel (#11)
    * fix job queue
    
    * give parameters as options in get
    
    * image data
    
    * add rdependancy earthengine
    
    * fix req
    
    * add image test
    
    * fix flask
    
    * fix cors import
    
    * fix tqdm import

--- api/image_get.py
+++ api/image_get.py
@@ -1,42 +1,51 @@
 import ee
-import math
+from pprint import pprint
 from datetime import datetime, timedelta
 
 ee.Authenticate()
-
 ee.Initialize(project='ee-neelrages')
 
 def get_satellite_images(latitude, longitude, radius=10):
-
     radius_meters = radius * 1609.34
-    
     point = ee.Geometry.Point([longitude, latitude])
-    roi = point.buffer(radius_meters) 
-    
-    start_date = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d')
+    roi = point.buffer(radius_meters)
+
+    # Extend date range
+    start_date = (datetime.now() - timedelta(days=90)).strftime('%Y-%m-%d')
     end_date = datetime.now().strftime('%Y-%m-%d')
-    
+
+    # Adjust cloud cover threshold
     collection = (
         ee.ImageCollection('COPERNICUS/S2_HARMONIZED')
         .filterBounds(roi)
         .filterDate(start_date, end_date)
-        .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 20))
-      )
+        .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 50))
+    )
+
+    # Print number of images
+    print('Number of images:', collection.size().getInfo())
 
     vis_params = {
         'min': 0,
         'max': 3000,
-        'bands': ['B4', 'B3', 'B2'], 
+        'bands': ['B4', 'B3', 'B2'],
     }
 
     image_urls = []
-    images = collection.toList(collection.size()) 
+    images = collection.toList(collection.size())
     for i in range(images.size().getInfo()):
         image = ee.Image(images.get(i))
-        url = image.getThumbURL({'region': roi, 'dimensions': 512, 'format': 'png', **vis_params})
+
+        # Increase image resolution
+        url = image.getThumbURL({
+            'region': roi.getInfo()['coordinates'],
+            'dimensions': 1024,
+            'format': 'png',
+            **vis_params
+        })
         image_urls.append(url)
 
     return image_urls
 
 images = get_satellite_images(43.000, -71.299)
-print(images)
+pprint(images)
