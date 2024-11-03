09964e22086408a2cd0dc0669e8939a85e4bb23b
neelthepatel <patel.neel5@northeastern.edu>
2024-11-03 00:44:43 -0400

    fix

--- api/index.py
+++ api/index.py
@@ -3,8 +3,8 @@ from flask_cors import CORS, cross_origin
 from api.urls import image_urls
 from api.smoke_dispersion import simulate_smoke_dispersion
 from api.Location import Location
-# from api.run_model import predict_img
-from pprint import pprint 
+from api.run_model import predict_img
+import random 
 
 # aa
 app = Flask(__name__)
@@ -27,6 +27,8 @@ def simulate():
         radius = data.get('radius', 3)
         delta = data.get('separation', 0.2)
 
+       
+
 
         if x0 is None or y0 is None:
             return jsonify({'error': 'Missing latitude or longitude'}), 400
@@ -34,6 +36,11 @@ def simulate():
         source = Location(x0, y0)
         
         grid = simulate_smoke_dispersion(source, radius, delta)
+
+        wildfire_images = []
+        for index, image in enumerate(random.sample(image_urls, 6)):
+            if predict_img(image) == "Wildfire" and index < len(grid):
+                wildfire_images.append((image, grid[index][0].x, grid[index][0].y))
         
         result = [{"chunk": index + 1, 'latitude': element[0].x, 'longitude': element[0].y, "dispersion": element[1]} for index, element in enumerate(grid)]
         
@@ -46,4 +53,4 @@ def simulate():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050)
+    app.run(host="0.0.0.0", port=5050, debug=True)
